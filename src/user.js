f4374e355cee5982af6922fb28b14b6bee7cefcb
Jimmy Lin <jmlntw@gmail.com>
2017-09-26 11:37:39 +0800

    Merge two if expressions

--- src/user.js
+++ src/user.js
@@ -5,16 +5,14 @@ GM_addStyle('.GM_favicon { margin-right: 0.5em; vertical-align: middle; }')
 const observer = new window.MutationObserver(mutations => {
   mutations.forEach(mutation => {
     const target = mutation.target
-    if (target.classList.contains('entry')) {
-      if (target.querySelector('.GM_favicon') === null) {
-        const source = target.querySelector('a.source')
-        if (source !== null) {
-          const domain = source.href.replace(/^https?:\/\/(?:www.)?([^/:]+).*/i, '$1')
-          const favicon = document.createElement('img')
-          favicon.src = `https://www.google.com/s2/favicons?domain=${domain}&alt=feed`
-          favicon.classList.add('GM_favicon')
-          source.insertAdjacentElement('afterbegin', favicon)
-        }
+    if (target.classList.contains('entry') && target.querySelector('.GM_favicon') === null) {
+      const source = target.querySelector('a.source')
+      if (source !== null) {
+        const domain = source.href.replace(/^https?:\/\/(?:www.)?([^/:]+).*/i, '$1')
+        const favicon = document.createElement('img')
+        favicon.src = `https://www.google.com/s2/favicons?domain=${domain}&alt=feed`
+        favicon.classList.add('GM_favicon')
+        source.insertAdjacentElement('afterbegin', favicon)
       }
     }
   })
