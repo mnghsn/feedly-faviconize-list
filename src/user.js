58b3627fb62d30a8de3b36e216bbbe8a8f93ed1f
Jimmy Lin <jmlntw@gmail.com>
2017-11-13 20:12:37 +0800

    Update favicon class name for consistency

--- src/user.js
+++ src/user.js
@@ -9,18 +9,18 @@ function GM_addStyle (css) {
   return style
 }
 
-GM_addStyle('.GM_favicon { margin-right: 0.5em; vertical-align: middle; }')
+GM_addStyle('.gm-favicon { margin-right: 0.5em; vertical-align: middle; }')
 
 const observer = new window.MutationObserver(mutations => {
   mutations.forEach(mutation => {
     const target = mutation.target
-    if (target.classList.contains('entry') && target.querySelector('.GM_favicon') === null) {
+    if (target.classList.contains('entry') && target.querySelector('.gm-favicon') === null) {
       const source = target.querySelector('a.source')
       if (source !== null) {
         const domain = source.href.replace(/^https?:\/\/(?:www.)?([^/:]+).*/i, '$1')
         const favicon = document.createElement('img')
         favicon.src = `https://www.google.com/s2/favicons?domain=${domain}&alt=feed`
-        favicon.classList.add('GM_favicon')
+        favicon.classList.add('gm-favicon')
         source.insertAdjacentElement('afterbegin', favicon)
       }
     }
