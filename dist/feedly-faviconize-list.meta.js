1535d32719afe0aa131935cacc4f234fc25fa062
Jimmy Lin <jmlntw@gmail.com>
2019-01-04 19:20:54 +0800

    Release v0.2.2

--- dist/feedly-faviconize-list.meta.js
+++ dist/feedly-faviconize-list.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Feedly Faviconize List
 // @namespace       jmln.tw
-// @version         0.2.0
+// @version         0.2.2
 // @description     A user script to show feed favicons in Feedly Title-Only View.
 // @author          Jimmy Lin
 // @license         MIT
