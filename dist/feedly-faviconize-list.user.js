713f65002438574b0e666b2c0add698c351f9c88
Jimmy Lin <jmlntw@gmail.com>
2019-03-12 17:38:26 +0800

    Release v0.2.3

--- dist/feedly-faviconize-list.user.js
+++ dist/feedly-faviconize-list.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Feedly Faviconize List
 // @namespace       jmln.tw
-// @version         0.2.2
+// @version         0.2.3
 // @description     A user script to show feed favicons in Feedly Title-Only View.
 // @author          Jimmy Lin
 // @license         MIT
@@ -23,21 +23,92 @@ function GM_addStyle (css) {
   return style
 }
 
-GM_addStyle('.gm-favicon { margin-right: 0.5em; vertical-align: middle; }')
+GM_addStyle(`
+  .gm-favicon {
+    width: 16px;
+    height: 16px;
+    margin: 0 6px 0 0;
+    padding: 0;
+    border-radius: 3px;
+    vertical-align: top;
+  }
+`)
 
-const observer = new MutationObserver(mutations => {
-  mutations.forEach(mutation => {
-    const target = mutation.target
-    if (target.classList.contains('entry') && target.querySelector('.gm-favicon') === null) {
-      const source = target.querySelector('a.source')
-      if (source !== null) {
-        const domain = source.href.replace(/^https?:\/\/([^/:]+).*/i, '$1')
-        const favicon = document.createElement('img')
-        favicon.src = `https://www.google.com/s2/favicons?domain=${domain}&alt=feed`
-        favicon.classList.add('gm-favicon')
-        source.insertAdjacentElement('afterbegin', favicon)
+function awaitSelector (selector, root) {
+  return new Promise((resolve, reject) => {
+    try {
+      const rootElement = root ?
+        typeof root === 'string' ? document.querySelector(root) : root :
+        document
+
+      const findAndResolveElements = () => {
+        const allElements = document.querySelectorAll(selector)
+        const newElements = []
+        const resolvedAttr = 'data-awaitselector-resolved'
+
+        if (allElements.length > 0) {
+          Array.prototype.slice.call(allElements)
+            .filter(element => typeof element[resolvedAttr] === 'undefined')
+            .forEach(element => {
+              element[resolvedAttr] = true
+              newElements.push(element)
+            })
+
+          if (newElements.length > 0) {
+            observer.disconnect()
+            resolve(newElements)
+          }
+        }
       }
+
+      const observer = new MutationObserver(mutations => {
+        const addedNodes = mutations.reduce((found, mutation) => {
+          return found || mutation.addedNodes && mutation.addedNodes.length > 0
+        })
+
+        if (addedNodes) {
+          findAndResolveElements()
+        }
+      })
+
+      observer.observe(rootElement, {
+        childList: true,
+        subtree: true
+      })
+
+      findAndResolveElements()
+    } catch (exception) {
+      reject(exception)
     }
   })
+}
+
+function waitAwaitSelector (selector, root, callback) {
+  (function awaiter () {
+    const continueWatching = arguments.length > 0 && arguments[0] !== void 0 ? arguments[0] : true
+
+    if (continueWatching) {
+      awaitSelector(selector, root).then(callback).then(awaiter)
+    }
+  }())
+}
+
+function createFavicon (url) {
+  const domain = url.replace(/^https?:\/\/([^/:]+).*/i, '$1')
+  const favicon = document.createElement('img')
+
+  favicon.src = `https://www.google.com/s2/favicons?domain=${domain}&alt=feed`
+  favicon.classList.add('gm-favicon')
+
+  return favicon
+}
+
+awaitSelector('#feedlyPageFX', '#box').then(pages => {
+  waitAwaitSelector('a.source[data-uri]', pages[0], sources => {
+    sources
+      .filter(source => source.querySelector('.gm-favicon') === null)
+      .forEach(source => {
+        source.insertAdjacentElement('afterbegin', createFavicon(source.href))
+      })
+  })
 })
-observer.observe(document.getElementById('box'), { childList: true, subtree: true })
